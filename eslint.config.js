f744a9968e98286e8e2503d312b86c4ccf9ab10a
maximum0 <maximum.zero95@gmail.com>
2025-06-02 14:48:53 +0900

    chore: 프로젝트 초기 설정 변경

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{ts,tsx}'], // 이 설정은 .ts, .tsx 파일에 적용
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module', // ES Modules로 파싱하도록 명시
+        ecmaFeatures: {
+          jsx: true, // JSX 지원
+        },
+      },
+      globals: globals.browser, // 브라우저 전역 변수
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <>
       <div>초기 세팅</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
