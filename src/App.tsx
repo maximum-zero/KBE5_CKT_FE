0246435778fbc50efb61b281f204788b9cc77243
maximum0 <maximum.zero95@gmail.com>
2025-06-07 19:13:40 +0900

    feat: 공통 로딩 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { ThemeContextProvider, useTheme } from '@/context/ThemeContext';
+import { LoadingProvider } from './context/LoadingContext';
 import GlobalStyle from '@/styles/GlobalStyle';
 import DashboardLayout from '@/layouts/DashboardLayout';
 
@@ -32,31 +33,33 @@ function App() {
   return (
     <ThemeContextProvider>
       <StyledThemeProviderWrapper>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
+        <LoadingProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
 
-            <Route element={<DashboardLayout title="차칸 관제시스템" />}>
-              <Route path="/" element={<RealtimeMonitoringPage />} />
+              <Route element={<DashboardLayout title="차칸 관제시스템" />}>
+                <Route path="/" element={<RealtimeMonitoringPage />} />
 
-              <Route path="/monitoring">
-                <Route index element={<DrivingLogPage />} />
-                <Route path="driving-log" element={<DrivingLogPage />} />
-                <Route path="driving-history" element={<DrivingHistoryPage />} />
+                <Route path="/monitoring">
+                  <Route index element={<DrivingLogPage />} />
+                  <Route path="driving-log" element={<DrivingLogPage />} />
+                  <Route path="driving-history" element={<DrivingHistoryPage />} />
+                </Route>
+
+                <Route path="/vehicle">
+                  <Route index element={<VehicleListPage />} />
+                  <Route path="list" element={<VehicleListPage />} />
+                </Route>
+
+                <Route path="/customer">
+                  <Route index element={<CustomerListPage />} />
+                  <Route path="list" element={<CustomerListPage />} />
+                </Route>
               </Route>
-
-              <Route path="/vehicle">
-                <Route index element={<VehicleListPage />} />
-                <Route path="list" element={<VehicleListPage />} />
-              </Route>
-
-              <Route path="/customer">
-                <Route index element={<CustomerListPage />} />
-                <Route path="list" element={<CustomerListPage />} />
-              </Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
+            </Routes>
+          </BrowserRouter>
+        </LoadingProvider>
       </StyledThemeProviderWrapper>
     </ThemeContextProvider>
   );
