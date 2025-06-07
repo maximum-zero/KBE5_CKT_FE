3cd033c582121fe5b2640fef2bd29d990d8a74d9
maximum0 <maximum.zero95@gmail.com>
2025-06-07 19:53:04 +0900

    style: 공통 Text Theme 사용 방식 변경

--- src/components/ui/text/Text.styles.ts
+++ src/components/ui/text/Text.styles.ts
@@ -6,36 +6,36 @@ export const BaseSpan = styled(motion.span)`
 `;
 
 export const HeadingText = styled(BaseSpan)`
-  color: ${({ theme }) => theme.gray900};
+  color: var(--color-gray900);
   font-size: 20px;
   font-weight: 700;
   line-height: 24px;
 `;
 
 export const SubheadingText = styled(BaseSpan)`
-  color: ${({ theme }) => theme.gray800};
+  color: var(--color-gray800);
   font-size: 16px;
   font-weight: 900;
   line-height: 19.2px;
 `;
 
 export const LabelText = styled(BaseSpan)`
-  color: ${({ theme }) => theme.gray700};
+  color: var(--color-gray700);
   font-size: 14px;
   font-weight: 500;
   line-height: 16.8px;
 `;
 
 export const PlaceholderText = styled(BaseSpan)`
-  color: ${({ theme }) => theme.gray500};
+  color: var(--color-gray500);
   font-size: 14px;
   font-weight: 400;
   line-height: 16.8px;
 `;
 
 export const ErrorText = styled(BaseSpan)`
-  color: ${({ theme }) => theme.red};
-  font-size: 14px;
+  color: var(--color-red);
+  font-size: 12px;
   font-weight: 500;
-  line-height: 16.8px;
+  line-height: 14px;
 `;
