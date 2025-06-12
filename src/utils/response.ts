44bffa814b9d38f0f5d5ec2c66662310be4dd4ba
maximum0 <maximum.zero95@gmail.com>
2025-06-13 07:28:38 +0900

    fix: API 공통 error 핸들링 처리

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -4,4 +4,22 @@ export interface CommonResponse<T> {
   message: string;
 }
 
+export interface CommonErrorResponse {
+  code: string;
+  message: string;
+}
+
 export const CODE_SUCCESS = '000';
+
+export class APIError extends Error {
+  public code: string;
+  public responseData: CommonErrorResponse;
+
+  constructor(response: CommonErrorResponse) {
+    super(response.message || '알 수 없는 서버 오류가 발생했습니다.');
+    this.name = 'ApiError';
+
+    this.code = response.code;
+    this.responseData = response;
+  }
+}
