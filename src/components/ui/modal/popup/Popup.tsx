fff74f536511ab900670c5f85465a1c107295ee0
maximum0 <maximum.zero95@gmail.com>
2025-06-19 13:48:03 +0900

    feat: 공통 팝업 외부 스크롤 막기 기능 추가

--- src/components/ui/modal/popup/Popup.tsx
+++ src/components/ui/modal/popup/Popup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AnimatePresence } from 'framer-motion';
+import { RemoveScroll } from 'react-remove-scroll';
 
 import { Overlay, PopupContainer, Header, ContentWrapper, ActionButtonsWrapper } from './Popup.styles';
 import type { BasicPopupProps } from './types';
@@ -11,27 +12,34 @@ import IconClose from '@/assets/icons/ic-close.svg?react';
 
 export const Popup: React.FC<BasicPopupProps> = ({ isOpen, onClose, title, children, actionButtons }) => {
   return (
-    <AnimatePresence>
-      {isOpen && (
-        <Overlay initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
-          <PopupContainer
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.8, opacity: 0 }}
-            transition={{ type: 'spring', stiffness: 300, damping: 25 }}
-            onClick={e => e.stopPropagation()}
+    <RemoveScroll enabled={isOpen}>
+      <AnimatePresence>
+        {isOpen && (
+          <Overlay
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
           >
-            <Header>
-              <Text type="heading2">{title}</Text>
-              <ActionButton icon={<IconClose />} buttonType="gray" onClick={onClose} aria-label="닫기" />
-            </Header>
+            <PopupContainer
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              transition={{ type: 'spring', stiffness: 300, damping: 25 }}
+              onClick={e => e.stopPropagation()}
+            >
+              <Header>
+                <Text type="heading2">{title}</Text>
+                <ActionButton icon={<IconClose />} buttonType="gray" onClick={onClose} aria-label="닫기" />
+              </Header>
 
-            <ContentWrapper>{children}</ContentWrapper>
+              <ContentWrapper>{children}</ContentWrapper>
 
-            {actionButtons && <ActionButtonsWrapper>{actionButtons}</ActionButtonsWrapper>}
-          </PopupContainer>
-        </Overlay>
-      )}
-    </AnimatePresence>
+              {actionButtons && <ActionButtonsWrapper>{actionButtons}</ActionButtonsWrapper>}
+            </PopupContainer>
+          </Overlay>
+        )}
+      </AnimatePresence>
+    </RemoveScroll>
   );
 };
