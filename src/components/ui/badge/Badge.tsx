02a2978fbbb59503c9bad3c71a2e3a1d47e42cf9
LeeHyunKyeong <s2s20p@naver.com>
2025-06-19 12:19:21 +0900

    feat: 배지 컴포넌트에 클릭 이벤트 추가

--- src/components/ui/badge/Badge.tsx
+++ src/components/ui/badge/Badge.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyledBadge, StyledBadgeTextSpan } from './Badge.styles';
 import type { BadgeProps } from './types';
 
-export const Badge: React.FC<BadgeProps> = ({ $badgeColor, children }) => {
+export const Badge: React.FC<BadgeProps> = ({ $badgeColor, children, onClick, clickable }) => {
   return (
-    <StyledBadge $badgeColor={$badgeColor}>
+    <StyledBadge $badgeColor={$badgeColor} onClick={onClick} $clickable={clickable}>
       <StyledBadgeTextSpan>{children}</StyledBadgeTextSpan>
     </StyledBadge>
   );
