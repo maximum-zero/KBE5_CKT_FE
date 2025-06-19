5ee901d64cf1903c396d5d7ef415d8ab5b0128f6
maximum0 <maximum.zero95@gmail.com>
2025-06-19 13:48:03 +0900

    feat: 공통 팝업 외부 스크롤 막기 기능 추가

--- src/components/ui/modal/popup/Popup.styles.ts
+++ src/components/ui/modal/popup/Popup.styles.ts
@@ -16,7 +16,8 @@ export const Overlay = styled(motion.div)`
 
 export const PopupContainer = styled(motion.div)`
   width: 600px;
-  padding: 24px;
+  max-height: calc(100vh - 64px);
+  padding: 24px 0;
   background: var(--color-white);
   border-radius: 8px;
   outline: 1px solid var(--color-gray600);
@@ -28,6 +29,7 @@ export const PopupContainer = styled(motion.div)`
 
 export const Header = styled.div`
   display: flex;
+  padding: 0 24px;
   justify-content: space-between;
   align-items: center;
   align-self: stretch;
@@ -36,13 +38,32 @@ export const Header = styled.div`
 export const ContentWrapper = styled.div`
   flex-grow: 1;
   display: flex;
+  padding: 0 24px;
   flex-direction: column;
   gap: 20px;
+
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background-color: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 4px;
+  }
+  &:hover::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+  scrollbar-width: thin;
+  scrollbar-color: rgba(0, 0, 0, 0.2) transparent;
 `;
 
 export const ActionButtonsWrapper = styled.div`
   align-self: stretch;
   display: flex;
+  padding: 0 24px;
   justify-content: flex-end;
   align-items: center;
   gap: 12px;
