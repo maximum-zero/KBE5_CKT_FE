ef0d5d17bab425621c77b0f6ce78ecf717b4f065
maximum0 <maximum.zero95@gmail.com>
2025-07-17 10:25:14 +0900

    chore: 불 필요한 로그 제거

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log('import.meta.env.VITE_API_WITH_CREDENTIALS > ', import.meta.env.VITE_API_WITH_CREDENTIALS);
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: import.meta.env.VITE_API_WITH_CREDENTIALS || true,
